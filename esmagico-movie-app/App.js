40921bddc13901aebd5529d5d5ef2467edc5eccf
Amrut Sagar <amrutsagar06@gmail.com>
2022-08-24 16:22:23 +0530

    omdb api & search bar

--- esmagico-movie-app/App.js
+++ esmagico-movie-app/App.js
@@ -1,11 +1,43 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
+import axios from "axios";
+import { useState } from "react";
 
 export default function App() {
+  const apiurl = "http://www.omdbapi.com/?i=tt3896198&apikey=91ca02dc";
+  const [state, setState] = useState({
+    s: "Enter a movie...",
+    results: [],
+    selected: {},
+  });
+
+  const search = () => {
+    //axios is making a call to our api url and on the end we re adding a new query parameter which is going to be s which stands for search in the database or the api we are using
+    //and was then equal to the search crew we have set up here so currently it will be set to enter movie but when we actually type like on screen next it says "Batman(movie name you want)" we can actually change the batman to be what hit enter and how we'll search for that word so it'd be Batman which pass through
+    axios(apiurl + "&s=" + state.s).then(({ data }) => {
+      let results = data.search;
+      console.log(results);
+      setState((prevState) => {
+        //we then going to return it and set the previous state to setState like how the state works we re then going to get the results and set the rsults to the rresults.
+        return { ...prevState, results: results };
+      });
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Omdb movie app es magico</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Movie Db</Text>
+      <TextInput
+        style={styles.searchbox}
+        onChangeText={(text) =>
+          setState((prevState) => {
+            return { ...prevState, s: text };
+          })
+        }
+        //means once you submitted it will allow us to do a search.
+        onSubmitEditing={search}
+        value={state.s}
+      />
     </View>
   );
 }
@@ -13,8 +45,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#223343",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
+    paddingTop: 70,
+    paddingHorizontal: 20,
+  },
+  title: {
+    color: "#FFF",
+    fontSize: 32,
+    fontWeight: "700",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  searchbox: {
+    fontSize: 20,
+    fontWeight: "300",
+    padding: 20,
+    width: "100%",
+    backgroundColor: "#FFF",
+    borderRadius: 8,
+    marginBottom: 40,
   },
 });
